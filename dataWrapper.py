7be9d7be42f5f419885573a7298a5187f15d9721
richardson.m <null>
2024-07-02 15:00:56 -0400

    update mol2parser

--- dataWrapper.py
+++ dataWrapper.py
@@ -1,34 +1,46 @@
+# Copyright (C) 2024 Matthew Richardson
+
 import pandas as pd
 import numpy as np
-import box_algorithm as box
+import boxFunctions as box
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Polypeptide import is_aa
+from mol2parser import mol2parser
 
 # Let Biopython handle parsing the PDB files
-file='demo.protein.pdw'
+file='testing_set/1YU6_C.pdb'
 parser=PDBParser()
 structure=parser.get_structure("my very first protein", file)
+residues=structure.get_residues()
+
+# Parse the corresponding mol2 file to grab some extra information
+mol2file=file+".mol2"
+mol2parser=mol2parser()
+mol2parser.parse(mol2file)
+mol2atoms=mol2parser.get_atoms()
 
 # Extract Atoms from the structure. We will need to refer back to them later.
+# This currently uses the Nitrogen as the center of each amino acid.
 atoms=[]
 centralAtoms=[]
-for residue in structure.get_residues():
-    if is_aa(residue): # Make sure the residue is an amino acid
+for residue in residues:
+    if True: #is_aa(residue): # Make sure the residue is an amino acid
         for index,atom in enumerate(residue.get_atoms()):
             if index==0: centralAtoms.append(atom)
             atoms.append(atom)
 
-# Extract Coordinates from Atom Objects
-centralCoords=[atom.get_coord() for atom in centralAtoms]
-atomCoords=[atom.get_coord() for atom in atoms]
-#print(centralCoords[0])
-
-# Run box generation algorithm
-for index,centralAtom in enumerate(centralCoords):
-    inRange=box.buildBox(centralAtom, 5, 1, atomCoords)
-    print(index,inRange)
-
-# Don't forget we need to exclude the central atom from the box!
     
+
+# Extract Coordinates from Atom Objects
+centralCoords=np.array([atom.get_coord() for atom in centralAtoms], dtype=np.float32)
+atomCoords=np.array([atom.get_coord() for atom in atoms], dtype=np.float32)
+#structureLimits=box.get_structureLimits(atomCoords)
+#samplePoints=box.get_samplePoints(structureLimits,1)
+
+
+
+
+
+
 
 ## May want to read Biopython 11.6.4.5 for a more efficient implementation
--- mol2parser.py
+++ mol2parser.py
@@ -5,36 +5,67 @@ The goal of this parser is to maintain interoperability with Biopython's parsing
 
 '''
 
+import numpy as np
+from Bio.PDB.PDBParser import PDBParser
 import re
 
 class mol2parser:
+    
     def __init__(self):
         self.parsedData=[]
+        self.sections=[]
+        self.atoms=[]
 
-    def parse(self, file):
+    def parse_mol2(self, file):
         
         with open(file, 'r') as mol2file:
-    
+            
             sectionCounter=-1
         
             delimiter=re.compile(r'\s+')
             
             for line in mol2file:
-                
+
                 line=line.strip()
                 
                 if line.startswith('@<TRIPOS>'):
                     sectionCounter+=1
                     self.parsedData.append([line.removeprefix('@<TRIPOS>')])
                     sectionName=self.parsedData[sectionCounter][0]
+                    self.sections.append([sectionName,sectionCounter])
             
                 elif line:
                     line=delimiter.split(line)      
                     self.parsedData[sectionCounter].append(line)
+
+            self.sections=dict(self.sections)
                             
-
                     
+    def get_atoms(self):
+        
+        return self.parsedData[self.sections['ATOM']][1:]
 
+    def parse_pdb(self, file):
 
+        pdb=PDBParser()
+        structure=pdb.get_structure(file, file)
+        
+        for residue in structure.get_residues():
+            for index,atom in enumerate(residue.get_atoms()):
+                if index==0:
+                    atom.central=True
+                else: 
+                    atom.central=False
+
+                self.atoms.append(atom)
+
+    def update_records(self):
+
+        mol2atoms=self.get_atoms()
+
+        for i in range(len(self.atoms)):
+            self.atoms[i].detailedAtomType=mol2atoms[i][5]
+
+        
 
 
