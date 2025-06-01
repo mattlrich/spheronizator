9b0f6a6d836853defecee95af5402138a4c782f4
richardson.m <null>
2025-06-01 09:54:49 -0400

    rename testing directory remove uneeded files

--- condaload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This is just a little helper script to facilitate getting our conda environment up and running.
-
-module load conda
-conda activate /blue/raquel.dias/share/conda/envs/proteinBox
